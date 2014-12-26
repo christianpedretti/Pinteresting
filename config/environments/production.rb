910e6969dccddd194c81203a73b2f87f1f302f39
Christian Pedretti <christianpedretti@gmail.com>
2014-12-26 13:09:47 -0600

    Implemented Devise for Users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,10 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Required for Heroku
+  # Note to set this as my actual host name.
+  config.action_mailer.default_url_options = { :host => 'peaceful-retreat-5505.herokuapp.com' }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
